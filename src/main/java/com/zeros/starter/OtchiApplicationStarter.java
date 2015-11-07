1a196b84fdad5a35e903f7af8f8cf6a1d2afb6b4
zeroual <abid1991@hotmail.fr>
2015-11-07 14:43:20 +0100

    init the app with mvn spring boot template
    add spring boot dependencies
    create project structure ( separation by domain )

---
+++ src/main/java/com/zeros/starter/OtchiApplicationStarter.java
@@ -0,0 +1,17 @@
+package com.zeros.starter;
+
+import com.zeros.config.ApplicationConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Configuration
+@Import(value = ApplicationConfig.class)
+public class OtchiApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtchiApplicationStarter.class, args);
+    }
+}
