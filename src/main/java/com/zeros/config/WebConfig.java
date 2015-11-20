edf099e8e129bb4b489a40d7dd1237d63ef76890
zeroual <abid1991@hotmail.fr>
2015-11-20 21:48:16 +0100

    add first front end example
    add karma
     separate controllers and services with the main js file app.js
     make the app depend to the kitchen module

---
+++ src/main/java/com/zeros/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.zeros.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan({"com.zeros.api"})
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
---
+++ src/main/webapp/app/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by jaifar on 29/10/15.
+ */
+angular.module('kitchen',['ngResource']);
+angular.module("otchi", ['kitchen']);
