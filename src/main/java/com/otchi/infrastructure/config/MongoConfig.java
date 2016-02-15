230aa7bd80d7a243f93287000c90cea418ce9f11
mr-jaifar <mr.jaifar@gmail.com>
2016-02-15 19:00:13 +0100

    FeedServiceImpl update

---
+++ src/main/java/com/otchi/infrastructure/config/MongoConfig.java
@@ -0,0 +1,27 @@
+package com.otchi.infrastructure.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * Created by MJR2 on 2/15/2016.
+ */
+
+@Configuration
+@EnableMongoRepositories("com.otchi.domaine")
+public class MongoConfig extends AbstractMongoConfiguration{
+
+
+    @Override
+    protected String getDatabaseName() {
+        return "otchiDB";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient();
+    }
+}
