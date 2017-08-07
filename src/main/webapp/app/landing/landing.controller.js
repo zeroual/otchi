af9fb753afa1cdfddbc42425d56db85eebc0e653
zeroual <abid1991@hotmail.fr>
2017-08-07 21:01:00 +0200

    Add mixpanel tracking to have more data about user experience (how user interact with app)

--- src/main/webapp/app/landing/landing.controller.js
+++ src/main/webapp/app/landing/landing.controller.js
@@ -2,13 +2,16 @@ angular.module('otchi')
     .controller('LandingController', function ($scope, $translate) {
 
         $scope.changeLanguage = function (langKey) {
-        var doc =  document.getElementById("otchi-page-container");
-            if(langKey == 'ar'){
+            var doc = document.getElementById("otchi-page-container");
+            if (langKey == 'ar') {
                 doc.style.direction = "rtl";
-            }else{
+            } else {
                 doc.style.direction = "ltr";
             }
             $translate.use(langKey);
+            track("change-language", {
+                language: $scope.usedLanguage()
+            });
         };
 
         $scope.usedLanguage = function () {
